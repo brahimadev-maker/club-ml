26e6303167f619b1ad7619dfcfbfcc7732449d4c
brahimadiarrassoubaesatic-create <brahima.diarrassouba.esatic@gmail.com>
2025-11-08 15:17:08 +0000

    current

--- server.js
+++ server.js
@@ -7,16 +7,24 @@ import postgres from 'postgres';
 const sql = postgres(process.env.DATABASE_URL);
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/register', async (req, res) => {
-    const { name, email, classe, password } = req.body;
+    const { name, email, whatsapp, classe, password } = req.body;
 
-    if (!name || !email || !classe || !password) {
+    if (!name || !email || !whatsapp || !classe || !password) {
         return res.status(400).json({ message: 'Tous les champs sont requis.' });
+    }
+
+
+    const whatsappRegex = /^(\+225|225)?(0[1|5|7])\d{8}$/;
+    if (!whatsappRegex.test(whatsapp)) {
+        return res.status(400).json({ 
+            message: 'Numéro WhatsApp invalide. Format attendu: +225 07/05/01 XX XX XX XX' 
+        });
     }
 
     try {
@@ -24,11 +32,10 @@ app.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         await sql`
-            INSERT INTO inscrit (nom, email, classe, mot_de_passe)
-            VALUES (${name}, ${email}, ${classe}, ${hashedPassword})
+            INSERT INTO inscrit (nom, email, whatsapp, classe, mot_de_passe)
+            VALUES (${name}, ${email}, ${whatsapp}, ${classe}, ${hashedPassword})
         `;
 
-     
         res.status(201).json({
             message: `Inscription réussie pour ${name} ! Bienvenue au Club ML.`,
         });
